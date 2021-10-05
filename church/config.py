d876b355487c7dd38683e56c9bd9dc42c0d3a598
Josia <josia-login@rosskopfs.de>
2021-10-05 13:51:33 +0200

    Config: short description

--- church/config.py
+++ church/config.py
@@ -1,2 +1,4 @@
+# Room/Calendar bookings: max. number of entries
 BOOKINGS_SEARCH_MAX = 20
+# Calendar: Maximum length of the description of an entry in the list
 CALENDAR_LIST_DESCRIPTION_LIMIT = 50
